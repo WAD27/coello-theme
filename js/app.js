9c5a52f8b0005b8446872654443bc8516c12b8fb
josecaos <jazzvoon@gmail.com>
2019-05-13 03:11:02 -0500

    Interactividad Header lista

--- js/app.js
+++ js/app.js
@@ -2,7 +2,11 @@ jQuery(document).ready(($) => {
 
   imgLiquid()
   home_slider()
-
+  //
+  $(window).on('scroll',()=>{
+    header()
+  })
+  //
 })
 //funciones
 function imgLiquid() {
@@ -29,4 +33,23 @@ function home_slider() {
     autoPlaySpeed: 1000
 
   });
+}
+
+
+function header() {
+  var top = jQuery(window).scrollTop()
+  var header = jQuery('nav#menu')
+  var maincontent = jQuery('#main')
+
+  if(top >= 32) {
+
+    maincontent.css('padding-top','100px')
+    header.css('position','fixed')
+
+  } else {
+
+    maincontent.css('padding-top','0px')
+    header.css('position','relative')
+  }
+
 }
