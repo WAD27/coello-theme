3d29344d371c1dc6480dd0f09d952f932bd55e11
josecaos <jazzvoon@gmail.com>
2019-05-13 20:07:59 -0500

    Template "Quienes Somos" + MOdulo header pagina

---
+++ template-parts/page-header.php
@@ -0,0 +1,18 @@
+<section id="page-header" class="container-fluid no-padding">
+
+  <div class="page-header container-fluid no-padding">
+    <div class="page-header-img col-12 imgLiquid imgLiquidFill">
+      <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="Pagina Coello Trejo">
+    </div>
+    <div class="page-header-title col-12">
+      <div class="row">
+        <div class="col-12 ha my-auto text-left">
+          <div class="container">
+            <h1 class="col-12"><?php echo the_title(); ?></h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</section>
