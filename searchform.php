9cd9692ed1fe16ec00df81c6952d2d5bcb54f4c2
josecaos <jazzvoon@gmail.com>
2019-06-26 22:13:42 -0500

    XXX

--- searchform.php
+++ searchform.php
@@ -1,9 +1,9 @@
 <div class="sidebar-search">
 
+  <h4 class="col-12 text-center">Busqueda</h4>
   <form action="search.php" method="get">
-    <label for="search">Busqueda</label>
-    <input type="text" name="search" id="search" value="<?php the_search_query(); ?>" />
-    <input type="image" alt="Busca" />
+    <input class="col-8" type="text" name="search" id="search" value="<?php the_search_query(); ?>" />
+    <input class="col-4" type="image" alt="Busca" />
   </form>
 
 </div>
